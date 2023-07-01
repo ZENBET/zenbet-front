daee8f243c2e30a4b367e14c046134432f6ab478
Sebastian Viana Leiva <svianal2@upao.edu.pe>
2023-07-01 00:34:03 -0500

    feat: Implementar la funcionalidad de actualizar competencia y crear su interfaz

--- src/app/actualizar-competencia/actualizar-competencia.component.ts
+++ src/app/actualizar-competencia/actualizar-competencia.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Competencia } from '../competencia';
+import { CompetenciaService } from '../competencia.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-actualizar-competencia',
   templateUrl: './actualizar-competencia.component.html',
   styleUrls: ['./actualizar-competencia.component.css']
 })
-export class ActualizarCompetenciaComponent {
+export class ActualizarCompetenciaComponent implements OnInit{
+
+  idCompetencia:number;
+  competencia: Competencia = new Competencia();
+  constructor(private competenciaService: CompetenciaService, private router: Router, private route: ActivatedRoute){ }
+
+  ngOnInit(): void {
+    this.idCompetencia = this.route.snapshot.params['idCompetencia'];
+    this.competenciaService.obtenerCompetenciaPorId(this.idCompetencia).subscribe(dato =>{
+      this.competencia = dato;
+    }, error => console.log(error));
+  }
+
+  irAlaListaCompetencia(){
+    this.router.navigate(['/competencias']);
+  }
+
+  onSumbit(){
+    this.competenciaService.actualizarCompetencia(this.competencia).subscribe(dato =>{
+      this.irAlaListaCompetencia();
+    }, error => console.log(error));
+  }
 
 }
